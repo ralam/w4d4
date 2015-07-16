ca599bd344c7611067966d552b81edd5dcc21a09
App Academy Student <student@appacademy.io>
2015-07-16 13:27:11 -0700

    Add album type column to album table

---
+++ Music/db/migrate/20150716202358_add_album_type_to_album.rb
@@ -0,0 +1,5 @@
+class AddAlbumTypeToAlbum < ActiveRecord::Migration
+  def change
+    add_column :albums, :album_type, :string
+  end
+end
--- Music/db/schema.rb
+++ Music/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716201708) do
+ActiveRecord::Schema.define(version: 20150716202358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150716201708) do
     t.integer  "band_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "album_type"
   end
 
   add_index "albums", ["band_id"], name: "index_albums_on_band_id", unique: true, using: :btree
