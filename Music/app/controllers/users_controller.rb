ed0465a4025f5c1e6b5b39c510de904a4629038b
App Academy Student <student@appacademy.io>
2015-07-16 10:49:26 -0700

    Create user controller

--- Music/app/controllers/users_controller.rb
+++ Music/app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to users_url
+    else
+      render :new
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    if current_user.nil?
+      redirect_to new_session_url
+      return
+    end
+    @user = current_user
+    render :show
+  end
+
 end
